3bbbc5d7b6a3eefa9aa75b1480c6c868c441a3a3
sirthebenjamin <126521851+sirthebenjamin@users.noreply.github.com>
2023-04-25 22:25:15 -0400

    Adss myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+// select the existing header element
+const oldHeader = document.querySelector('main#main');
+
+// remove the existing header element from the DOM
+oldHeader.remove();
+
+// create a new header element
+const newHeader = document.createElement('h1');
+
+// set the new header element's id to 'victory'
+newHeader.setAttribute('id', 'victory');
+
+// set the text of the new header element
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// add the new header element to the document's body
+document.body.appendChild(newHeader);
